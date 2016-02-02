eabf3c885d09f3240e5b702cde8a352ab682591d
Eva Simon <eva.simon@gmail.com>
2016-02-01 23:38:00 -0600

    add README file; refactor app.js

---
+++ README.md
@@ -0,0 +1,21 @@
+## Frogger
+
+This game has a Player and Enemies (Bugs). The goal of the player is to reach the water, without colliding into any of the enemies. The player can move left, right, up and down. The player has 3 lives represented with 3 small hearst in the right upper corner of the canvas. The enemies move in varying speeds on the paved block portion of the screen.
+
+20 points are gained once the player reaches the water then the player moves back to the start square. After every 100 points you move one level up where the enemies speed increases.
+
+Once the player collides with an enemy, looses one life and the player moves back to the start square. After loosing the third life, the game is over.
+
+### How to Start Game
+
+1. Open `index.html`
+
+### How to Play
+
+1. Use arrow keys to control the palyer
+2. Reach the water avoiding the enemies
+3. Get 20 points if you reach the water
+4. Gain 100 points to move up a level
+4. Loose a life when colliding with an enemy
+5. After loosing the 3rd life the game is over!
+
--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+// Game configuration parameters
+var config = {
+    maxCanvasX: 505, // maximum width of the canvas
+    blockOffset: 65, // paved rock block distance from top of the canvas
+    blockHeight: 83, // paved rock block height
+    blockWidth: 101, // paved rock block width
+    levelSpeed: 100, // enemy speed multiplier for each level
+    initialPlayerX: 202, // player's initial x position
+    initialPlayerY: 400, // player's initial y position
+    winPoints: 20, // points gained for reaching the water
+    levelPoints: 100, // points needed to move up a level
+    numberEnemies: 3 // the number of enemies
+};
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -18,21 +32,21 @@ Enemy.prototype.update = function(dt) {
     this.x += this.speed * dt;
 
     //Reset enemy position once they leave the canvas
-    if (this.x > 505) {
+    if (this.x > config.maxCanvasX) {
         this.reset();
     }
     // Collision
-    if ((this.x + 75) >= player.x && this.x <= (player.x + 101) &&
-        player.y > this.y && player.y < (this.y + 10)) {
+    if ((this.x + config.blockHeight) >= player.x && this.x <= (player.x + config.blockWidth) &&
+        player.y > this.y && player.y < (this.y + config.blockHeight)) {
         player.reset(false);
     }
 };
 
 // Set enemy initial position and speed
 Enemy.prototype.reset = function() {
-    this.x = -101;
-    this.y = 65 + Math.floor(Math.random() * 3) * 83;
-    this.speed = Math.floor(Math.random() * 4) * 20 + 100 * player.level;
+    this.x = -config.blockWidth;
+    this.y = config.blockOffset + Math.floor(Math.random() * 3) * config.blockHeight;
+    this.speed = Math.floor(Math.random() * 4) * 20 + config.levelSpeed * player.level;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -46,8 +60,8 @@ Enemy.prototype.render = function() {
 
 // Player and its properties
 var Player = function() {
-    this.x = 202;
-    this.y = 400;
+    this.x = config.initialPlayerX;
+    this.y = config.initialPlayerY;
     this.sprite = 'images/char-boy.png';
     this.spriteLife = 'images/Heart-small.png';
     this.score = 0;
@@ -62,16 +76,16 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
     // Set white overlay rectangle
-    ctx.fillStyle = "#fff";
-    ctx.fillRect(0, 0, 505, 50);
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, config.maxCanvasX, 50);
 
     // Set the text defaults
-    ctx.fillStyle = "#ff0000";
-    ctx.font = "30px Courier New";
+    ctx.fillStyle = '#ff0000';
+    ctx.font = '30px Courier New';
 
     // Draw the score and level on the canvas
-    ctx.fillText(player.score, 10, 40);
-    ctx.fillText('Level ' + player.level, 190, 40);
+    ctx.fillText(this.score, 10, 40);
+    ctx.fillText('Level ' + this.level, 190, 40);
 
     //  Draw the hearts on the canvas
     for (var i = 0; i < this.lives; i++) {
@@ -84,25 +98,25 @@ Player.prototype.handleInput = function(key) {
 
     switch (key) {
         case 'left': // move player to the left
-            if (this.x - 101 >= 0) {
-                this.x -= 101;
+            if (this.x - config.blockWidth >= 0) {
+                this.x -= config.blockWidth;
             }
             break;
         case 'up': // move player up
-            this.y -= 83;
+            this.y -= config.blockHeight;
             if (this.y < 0) {
                 // reset player position when reaches the water
                 this.reset(true);
             }
             break;
         case 'right': // move player to the right
-            if (this.x + 101 < 505) {
-                this.x += 101;
+            if (this.x + config.blockWidth < config.maxCanvasX) {
+                this.x += config.blockWidth;
             }
             break;
         case 'down': // move player down
-            if (this.y + 83 <= 400) {
-                this.y += 83;
+            if (this.y + config.blockHeight <= config.initialPlayerY) {
+                this.y += config.blockHeight;
             }
             break;
     }
@@ -110,13 +124,14 @@ Player.prototype.handleInput = function(key) {
 
 // Reset player position
 Player.prototype.reset = function(win) {
-    this.x = 202;
-    this.y = 400;
+    this.x = config.initialPlayerX;
+    this.y = config.initialPlayerY;
+
     if (win) {
         // increase score on win
-        this.score += 20;
+        this.score += config.winPoints;
         //increase level on every 100 points
-        if (this.score % 100 === 0) {
+        if (this.score % config.levelPoints === 0) {
             this.level++;
         }
     } else {
@@ -139,7 +154,7 @@ Player.prototype.reset = function(win) {
 var allEnemies = [];
 var player = new Player();
 // Create 3 enemies
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i < config.numberEnemies; i++) {
     allEnemies.push(new Enemy());
 }
 
